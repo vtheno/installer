cc4d34225206d15321b2ce37f48439cda6dde670
thautawarm <twshere@outlook.com>
2020-02-24 21:41:18 +0900

    two projects to solve multi-stage

--- actual_setup.py
+++ actual_setup.py
@@ -1,18 +1,12 @@
 from setuptools import setup, find_packages
-import sys
-import os
 from pathlib import Path
-from subprocess import call
-dir = Path(__file__).parent.absolute()
-os.chdir(str(dir))
-call([sys.executable, 'actual_setup.py', 'install'])
-call([sys.executable, 'install_binary.py'])
 
 version = 0.1
-readme = ''
+with Path('README.md').open() as readme:
+    readme = readme.read()
 
 setup(
-    name='pspy-placeholder',
+    name='pspy-proxy',
     version=version if isinstance(version, str) else str(version),
     keywords="",  # keywords of your project that separated by comma ","
     description="",  # a concise introduction of your project
@@ -23,8 +17,11 @@ setup(
     url='https://github.com/purescript-python/installer',
     author='thautawarm',
     author_email='twshere@outlook.com',
-    packages=['pspy_placeholder'],
-    install_requires=[],
+    packages=['pspy_proxy'],
+    entry_points={"console_scripts": ['pspy-blueprint=pspy_proxy.proxy:main']},
+    # above option specifies what commands to install,
+    # e.g: entry_points={"console_scripts": ["yapypy=yapypy.cmd:compiler"]}
+    install_requires=['purescripto>=0.7.3', 'requests'],  # dependencies
     platforms="any",
     classifiers=[
         "Programming Language :: Python :: 3.5",
