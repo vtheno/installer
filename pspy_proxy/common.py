3e2bb7b14d483086e916428e9042706818fbb7ec
vtheno <o@0oo.ooo>
2020-02-24 22:18:28 +0800

    FIX typo

--- pspy_proxy/common.py
+++ pspy_proxy/common.py
@@ -1,4 +1,4 @@
 from pathlib import Path
-import sys.executeable as execPATH
+from sys import executable as execPATH
 # out_path = Path('~/.pspy/bin').expanduser()
 out_path = Path(execPATH).parent.expanduser()
