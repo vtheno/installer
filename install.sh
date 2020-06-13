2d44c41f5b514627011db7d027170ed4009c44dc
thautwarm <twshere@outlook.com>
2020-06-13 17:37:26 +0900

    up

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ then
     exit 1
 fi
 
-[ ! -x "$(command -v pspy)" ] && pip install -U purescripto -y --no-cache
+[ ! -x "$(command -v pspy)" ] && pip install -U --no-cache purescripto -y
 
 td="`mktemp -d`"
 
